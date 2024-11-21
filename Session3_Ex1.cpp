377b8f82de3ab77b8e92730dc67169c26f9ba5b2
vvd-110106 <doanvuvan411@gmail.com>
2024-11-21 21:56:34 +0700

    Session3_Ex1

---
+++ Session3_Ex1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	// khai bao bien
+	char ten[50];
+	// nhap ten nguoi dung
+	printf("Ban hay nhap ten cua ban nhe: ");
+	scanf("%s", &ten);
+	// in cau xin chao ten ra man hinh
+	printf("Xin chao %s nhe.", ten);
+	return 0;
+}
