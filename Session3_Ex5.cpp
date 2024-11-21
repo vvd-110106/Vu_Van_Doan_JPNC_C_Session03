2b4eec2d915895c41f255c50d612ba585f41852f
vvd-110106 <doanvuvan411@gmail.com>
2024-11-21 23:44:33 +0700

    Session3_Ex5

---
+++ Session3_Ex5.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+
+int main(){
+	printf("                               Danh Sach Sinh Vien                                               ");
+	
+	printf("|Stt|       Ho va Ten         |     Tuoi  |       SDT              |          mail            |\n");
+	
+	printf("_______________________________________________________________________________________________\n");
+	
+	printf("| 1 |       Tran Van A      |     19    |        0123456789        |   vn@gmail.com           |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 2 |       Vu Phuong Anh   |     18    |        0123456789        |   bahb@gmail.com         |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 3 |       Dinh Viet Hung  |     27    |        0356584545        |   ab@gmail.com           |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 4 |       Vu Thi Trang    |     25    |        0123456789        |   cn@gmail.com           |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 5 |       Le Quoc Binh    |     25    |        0123456789        |   ok@gmail.com           |\n");
+	printf("_  __ ___ _ __ _  __  __ _ _ _  __ _  _ __  _ __  _ _ _ __ _ _ _ _  __ _ _ _  __ _  __  __ _ __ \n");
+	printf("| 6 |       Pham Thanh Dat  |     27    |        0356584545        |   hk@gmail.com           |\n");
+	return 0;
+	
+}
